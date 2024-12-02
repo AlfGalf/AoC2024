4a7622d646c56ccdd172daab0a315330a1a76292
Alfie Richards <main@alfierichards.com>
2024-12-02 23:19:20 +0000

    Day 2

---
+++ src/bin/day1.rs
@@ -0,0 +1,50 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("../../inputs/day1.txt");
+
+    let pairs: Vec<(usize, usize)> = input
+        .lines()
+        .map(|l| {
+            let mut nums = l.split(" ").filter(|s| !s.is_empty());
+            (
+                nums.next().unwrap().parse().unwrap(),
+                nums.next().unwrap().parse().unwrap(),
+            )
+        })
+        .collect();
+
+    let mut arrays: (Vec<usize>, Vec<usize>) = pairs.into_iter().unzip();
+
+    arrays.0.sort_unstable();
+    arrays.1.sort_unstable();
+
+    println!(
+        "{}",
+        arrays
+            .0
+            .iter()
+            .zip(&arrays.1)
+            .map(|(a, b)| a.abs_diff(*b))
+            .sum::<usize>()
+    );
+
+    let arrays: (HashMap<usize, usize>, HashMap<usize, usize>) = (
+        arrays.0.iter().fold(HashMap::new(), |mut acc, k| {
+            acc.entry(*k).and_modify(|frq| *frq += 1).or_insert(1);
+            acc
+        }),
+        arrays.1.iter().fold(HashMap::new(), |mut acc, k| {
+            acc.entry(*k).and_modify(|frq| *frq += 1).or_insert(1);
+            acc
+        }),
+    );
+
+    let ans: usize = arrays
+        .0
+        .iter()
+        .map(|(k, f)| k * f * arrays.1.get(&k).unwrap_or(&0))
+        .sum();
+
+    println!("{}", ans);
+}
---
+++ src/bin/day2.rs
@@ -0,0 +1,45 @@
+fn main() {
+    let input = include_str!("../../inputs/day2.txt");
+
+    let pairs = input
+        .lines()
+        .map(|l| {
+            let nums = l.split(" ").filter(|s| !s.is_empty());
+            nums.into_iter()
+                .map(|s| s.parse::<usize>().unwrap())
+                .collect::<Vec<usize>>()
+        })
+        .collect::<Vec<Vec<usize>>>();
+
+    fn check(v: &Vec<usize>) -> bool {
+        v.iter()
+            .zip(v.iter().skip(1))
+            .all(|(a, b)| a > b && a - b <= 3)
+            || v.iter()
+                .zip(v.iter().skip(1))
+                .all(|(a, b)| b > a && b - a <= 3)
+    }
+
+    let res = pairs.iter().filter(|l| check(&l)).count();
+
+    println!("{res}");
+
+    let res2 = &pairs
+        .iter()
+        .filter(|l| {
+            if check(&l) {
+                return true;
+            }
+            (0..l.len()).any(|i| {
+                check(
+                    &l.iter()
+                        .enumerate()
+                        .filter_map(|(ind, v)| if ind != i { Some(*v) } else { None })
+                        .collect(),
+                )
+            })
+        })
+        .count();
+
+    println!("{res2}");
+}
